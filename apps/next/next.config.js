c6e25d2f6e47b8c1c4f7d6f1e4132b0de0a00113
David Leuliette <dleuliette@gmail.com>
2022-05-20 16:13:44 +0200

    failed to compile

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,5 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const reactWebpackConfig = require('@nrwl/react/plugins/webpack');
 const withNx = require('@nrwl/next/plugins/with-nx');
+const { withExpo } = require('@expo/next-adapter');
+const withFonts = require('next-fonts');
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([
+  'react-native-web',
+  'react-native-svg',
+  'native-base',
+  'react-native-svg',
+]);
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -10,6 +19,26 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  webpack: (config, options) => {
+    config = reactWebpackConfig(config);
+    let reactNativeKey = config.resolve.alias['react-native'];
+    if (!reactNativeKey) {
+      config.resolve.alias['react-native'] = 'react-native-web';
+      config.resolve.alias['react-native$'] = 'react-native-web';
+      config.resolve.extensions.unshift('.web.js');
+    }
+    return config;
+  },
 };
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(
+  withPlugins(
+    [
+      withTM,
+      [withFonts, { projectRoot: __dirname }],
+      [withExpo, { projectRoot: __dirname }],
+      // your plugins go here.
+    ],
+    nextConfig
+  )
+);
--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,17 +1,18 @@
+import { NativeBaseProvider } from 'native-base';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NativeBaseProvider>
       <Head>
         <title>Welcome to next!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </NativeBaseProvider>
   );
 }
 
